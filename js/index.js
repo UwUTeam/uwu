479c75929f541de144d5ad68bbfb6f9e8b21653d
CLINIOI <igor230509@gmail.com>
2024-09-07 16:32:50 +0300

    9
    Co-Authored-By: uwugy <166118058+uwugy@users.noreply.github.com>

---
+++ js/index.js
@@ -0,0 +1,78 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Находим все dropdown элементы
+  const dropdowns = document.querySelectorAll(".dropdown");
+
+  // Для каждого dropdown элемента
+  dropdowns.forEach(function (dropdown) {
+    const dropdownButton = dropdown.querySelector(".btn-secondary");
+    const dropdownMenu = dropdown.querySelector(".dropdown-menu.drop-content");
+
+    // Функция для добавления или удаления класса 'active'
+    function toggleActiveClass() {
+      if (dropdownMenu.classList.contains("show")) {
+        dropdownButton.classList.add("active");
+      } else {
+        dropdownButton.classList.remove("active");
+      }
+    }
+
+    // Отслеживаем изменения в атрибутах класса
+    const observer = new MutationObserver(toggleActiveClass);
+
+    // Начинаем наблюдение за элементом dropdownMenu
+    observer.observe(dropdownMenu, {
+      attributes: true, // Наблюдаем за изменениями атрибутов
+      attributeFilter: ["class"], // Только для изменений класса
+    });
+  });
+});
+
+var swiper = new Swiper(".blog-slider", {
+  spaceBetween: 30,
+  effect: "fade",
+  loop: true,
+  mousewheel: {
+    invert: false,
+  },
+  // autoHeight: true,
+  pagination: {
+    el: ".blog-slider__pagination",
+    clickable: true,
+  },
+});
+
+document.getElementById("questions-btn").addEventListener("click", function () {
+  this.classList.add("active");
+  document.getElementById("requirements-btn").classList.remove("active");
+});
+
+document
+  .getElementById("requirements-btn")
+  .addEventListener("click", function () {
+    this.classList.add("active");
+    document.getElementById("questions-btn").classList.remove("active");
+  });
+
+var swiper = new Swiper(".mySwiper", {
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + "</span>";
+    },
+  },
+});
+
+const burger = document.querySelector(".navbar-burger");
+const menu = document.querySelector(".navbar-menu");
+const iconList = burger.querySelector(".bi-list");
+const iconX = burger.querySelector(".bi-x-lg");
+
+burger.addEventListener("click", () => {
+  burger.classList.toggle("is-active");
+  menu.classList.toggle("is-active");
+
+  // Переключаем видимость иконок
+  iconList.classList.toggle("d-none");
+  iconX.classList.toggle("d-none");
+});
